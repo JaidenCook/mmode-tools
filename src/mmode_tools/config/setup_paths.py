f56ef6f19c2e10b5e8783338783a988b517ac0b3
JaidenCook <jaiden.cook1@gmail.com>
2026-01-21 13:18:59 +0800

    Fixed base directory issue.

--- src/mmode_tools/config/setup_paths.py
+++ src/mmode_tools/config/setup_paths.py
@@ -3,27 +3,32 @@ import toml
 import shutil
 import importlib.resources as resources
 
-HOMEDIR = os.path.expanduser("~")
 configFile = "default_config.toml"
 mmodeConfigPath = "mmode_tools.config"
 
 def ensure_output_dirs():
+    """
+    ensure_output_dirs _summary_
+    """
+    from mmode_tools.io import BASEDIR
     # Load the default config file from package resources
     with resources.files(mmodeConfigPath).joinpath(configFile).open("r") as f:
         config = toml.load(f)
-
+    #
     directoryDict = config.get("paths", {})
-
+    #
     for _,path in directoryDict.items():
-        if not os.path.exists(HOMEDIR+path):
-            os.makedirs(HOMEDIR+path,exist_ok=True)
-            print(f"Created directory: {HOMEDIR+path}")
+        tempPath = BASEDIR/path
+        if not tempPath.exists():
+            tempPath.mkdir(parents=True,exist_ok=True)
+            print(f"Created directory: {tempPath._str}")
 
 
 def copy_array_data_files():
     """
     Move data files to the appropriate directories.
     """
+    from mmode_tools.io import BASEDIR
     with resources.files(mmodeConfigPath).joinpath(configFile).open("r") as f:
         config = toml.load(f)
     
@@ -37,26 +42,26 @@ def copy_array_data_files():
         # Example array layout text files.
         if file.endswith('.txt'):
             srcPath = dataPath.joinpath(file)
-            destPath = HOMEDIR + interferometrPath + file
+            destPath = BASEDIR / interferometrPath / file
             if not os.path.exists(destPath):
                 shutil.copy2(srcPath,destPath)
         # Example interferometer configuration file.
         if file == "N32_config.toml":
             srcPath = dataPath.joinpath(file)
-            destPath = HOMEDIR + interferometrPath + file
+            destPath = BASEDIR / interferometrPath / file
             if not os.path.exists(destPath):
                 shutil.copy2(srcPath,destPath)
         # Getting the example covariance tensors.
         if file.endswith('.toml') or file.endswith('.h5'):
             srcPath = dataPath.joinpath(file)
-            destPath = HOMEDIR + covTensorPath + file
+            destPath = BASEDIR / covTensorPath / file
             if not os.path.exists(destPath):
                 shutil.copy2(srcPath,destPath)
 
     # Move the test beams as well. These are just EDA2 beam models.
     for file in ["beam_model_XX_150MHz.fits","beam_model_YY_150MHz.fits"]:
         srcPath = dataPath.joinpath(file)
-        destPath = HOMEDIR + beamPath + file
+        destPath = BASEDIR / beamPath / file
         if not os.path.exists(destPath):
             shutil.copy2(srcPath,destPath)
     
