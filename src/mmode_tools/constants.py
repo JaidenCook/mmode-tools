2f9fcd3f849eb59b67ce22db240c3ca6762cae40
JaidenCook <jaiden.cook1@gmail.com>
2026-01-21 16:07:02 +0800

    Added boltzmans constant to constants

--- src/mmode_tools/constants.py
+++ src/mmode_tools/constants.py
@@ -1,7 +1,8 @@
 from astropy.coordinates import EarthLocation
 from astropy import units
 
-c = 299792458
+c = 299792458 # m s^-1
+kb = 1380.64 # K^-1 Jy
 MRO = EarthLocation.of_site('Murchison Widefield Array')
 ONSALA = EarthLocation.from_geocentric(3370272.092,712125.596,5349990.934,
                                        unit=units.m)
