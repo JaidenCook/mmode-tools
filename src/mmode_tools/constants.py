011ab0f234994486c089e313d70a37d88808ee31
JaidenCook <jaiden.cook1@gmail.com>
2026-01-22 10:53:42 +0800

    Added siderealDay2SolarDay to constants

--- src/mmode_tools/constants.py
+++ src/mmode_tools/constants.py
@@ -3,6 +3,7 @@ from astropy import units
 
 c = 299792458 # m s^-1
 kb = 1380.64 # K^-1 Jy
+siderealDay2SolarDay = 23.9345/24
 MRO = EarthLocation.of_site('Murchison Widefield Array')
 ONSALA = EarthLocation.from_geocentric(3370272.092,712125.596,5349990.934,
                                        unit=units.m)
