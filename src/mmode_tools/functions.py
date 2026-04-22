a4a7db1435cc5b6a65dd2cf6c35c4f3c6c472228
JaidenCook <jaiden.cook1@gmail.com>
2026-04-22 10:21:57 +0800

    Added amplitude normalisation.

--- src/mmode_tools/functions.py
+++ src/mmode_tools/functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def Gaussian2Dxy(xdata_tuple,amplitude,x0,y0,amaj,bmin,theta):
+def Gaussian2Dxy(xdata_tuple,amplitude,x0,y0,amaj,bmin,theta,
+                 normAmp=False):
     """
     Generates 2D Gaussian array.
 
@@ -34,4 +35,6 @@ def Gaussian2Dxy(xdata_tuple,amplitude,x0,y0,amaj,bmin,theta):
     b = -np.sin(2.0*theta)/(4.0*sigx**2) + np.sin(2.0*theta)/(4.0*sigy**2)    
     c = (np.sin(theta)**2)/(2.0*sigx**2) + (np.cos(theta)**2)/(2.0*sigy**2)
         
+    if normAmp:
+        amplitude = amplitude/(2.0*np.pi*sigx*sigy)
     return amplitude*np.exp(-(a*(X-x0)**2 + 2*b*(X-x0)*(Y-y0) + c*(Y-y0)**2))
